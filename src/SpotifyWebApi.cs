af8e5e0871cc3c8ff983a5efc409b31a9fe75a62
Daniel Larsen <dalars@microsoft.com>
2018-06-22 20:59:40 +1200

    Packing v1.0.1-alpha

---
+++ src/SpotifyWebApi.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading.Tasks;
+
+namespace SpotifyApiDotNetCore
+{
+    public class SpotifyWebApi : ISpotifyWebApi
+    {
+        public Task<dynamic> SearchArtists(string artist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> SearchArtists(string artist, int limit = 50)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetPlaylists(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetPlaylists(string username, int offset = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetRecommendation(string artistSeed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetRecommendation(string artistSeed, int limit = 20)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetRelatedArtists(string artistId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<dynamic> GetArtist(string artistId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PlayArtist(string userHash, string spotifyUri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
