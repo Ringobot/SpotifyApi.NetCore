15aaa209f6da073bad841626f1cfbaf0f0481bfe
Daniel Larsen <dalars@microsoft.com>
2018-06-22 20:01:36 +1200

    dotnet new

---
+++ src/ISpotifyWebApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SpotifyApiDotNetCore
+{
+    public interface ISpotifyWebApi
+    {
+        Task<dynamic> SearchArtists(string artist);
+        Task<dynamic> SearchArtists(string artist, int limit);
+
+        Task<dynamic> GetPlaylists(string username);
+        Task<dynamic> GetPlaylists(string username, int offset);
+
+        Task<dynamic> GetRecommendation(string artistSeed);
+        Task<dynamic> GetRecommendation(string artistSeed, int limit);
+
+        Task<dynamic> GetRelatedArtists(string artistId);
+
+        Task<dynamic> GetArtist(string artistId);
+
+        Task PlayArtist(string userHash,  string spotifyUri);
+    }
+}
