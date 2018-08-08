5597737bc29872ba4cec56b396037d0a161a5d7a
Daniel Larsen <dalars@microsoft.com>
2018-08-08 21:29:21 +1200

    New constructor for Spotify Api Exception

--- src/SpotifyApi.NetCore/Authorization/IBearerTokenStore.cs
+++ src/SpotifyApi.NetCore/Authorization/IBearerTokenStore.cs
@@ -12,6 +12,7 @@ namespace SpotifyApi.NetCore
 
     public interface IRefreshTokenStore
     {   
+        [Obsolete]  //TODO: Up to the consumer how they store refresh tokens returned by the service
         Task InsertOrReplace(string userHash, string token);
         
         Task<string> Get(string userHash);
