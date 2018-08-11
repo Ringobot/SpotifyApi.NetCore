e890d249347c8e5ed2421169033df18e0880aaa3
Daniel Larsen <dalars@microsoft.com>
2018-08-11 20:54:25 +1200

    Spotify Exception can handle more error objects and is more resilient

--- src/SpotifyApi.NetCore.Tests/SpotifyApiErrorExceptionTests.cs
+++ src/SpotifyApi.NetCore.Tests/SpotifyApiErrorExceptionTests.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Moq.Protected;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SpotifyApi.NetCore.Http;
 using SpotifyApi.NetCore.Tests.Http;
 
@@ -61,11 +63,48 @@ namespace SpotifyApi.NetCore.Tests
                 Content = new StringContent(content, Encoding.Unicode,"application/json")
             };
 
+            var j = JsonConvert.DeserializeObject(content) as JObject;
+            var t = j["error"].Type;
+            Debug.WriteLine($"error type = {t}");
+            Debug.WriteLine($"error.message = {j["error"].Value<string>("message")}");
+            Debug.WriteLine($"error.foo = {j["error"].Value<string>("foo")}");
+
             // act
             var error = await SpotifyApiErrorException.ReadErrorResponse(response);
 
             // assert
             Assert.IsNotNull(error);
         }
+
+        [TestMethod]
+        public async Task ReadErrorResponse_AlternateErrorFormat_ReturnsNotNull()
+        {
+            // arrange
+            const string content = "{\"error\":\"invalid_grant\",\"error_description\":\"Invalid authorization code\"}";
+
+            var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent(content, Encoding.Unicode,"application/json")
+            };
+
+            var j = JsonConvert.DeserializeObject(content) as JObject;
+            var t = j["error"].Type;            
+            Debug.WriteLine($"error type = {t}");
+            Debug.WriteLine($"error_description = {j["error_description"].Value<string>()}");
+
+            if (j.ContainsKey("foo"))
+            {
+                var t2 = j["foo"].Type;            
+                Debug.WriteLine($"foo type = {t2}");
+            }
+
+            // act
+            var error = await SpotifyApiErrorException.ReadErrorResponse(response);
+
+            // assert
+            Assert.IsNotNull(error);
+        }
+
+        // 
     }
 }
--- src/SpotifyApi.NetCore/Authorization/IBearerTokenStore.cs
+++ src/SpotifyApi.NetCore/Authorization/IBearerTokenStore.cs
@@ -10,11 +10,13 @@ namespace SpotifyApi.NetCore
         Task<BearerAccessToken> Get(string key);
     }
 
+    //TODO: -> IRefreshTokenProvider?
     public interface IRefreshTokenStore
     {   
         [Obsolete]  //TODO: Up to the consumer how they store refresh tokens returned by the service
         Task InsertOrReplace(string userHash, string token);
         
+        //TODO -> GetRefreshToken
         Task<string> Get(string userHash);
     }
 }
