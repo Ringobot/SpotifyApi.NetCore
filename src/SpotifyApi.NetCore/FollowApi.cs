af28fe4d6932cc820af707f314b7b25069c30fd6
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2020-06-23 17:28:54 +0530

    Added Follow Api one call
    Added the Follow Api call for checking by artist id or user id if the current logged in user is following them.

---
+++ src/SpotifyApi.NetCore/FollowApi.cs
@@ -0,0 +1,84 @@
+﻿using SpotifyApi.NetCore.Authorization;
+using SpotifyApi.NetCore.Helpers;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SpotifyApi.NetCore
+{
+    /// <summary>
+    /// An API Wrapper for the Spotify Web API Follow endpoints.
+    /// </summary>
+    public class FollowApi : SpotifyWebApi, IFollowApi
+    {
+        public enum ContainsTypes
+        {
+            Artist,
+            User
+        }
+
+        protected internal string GetCommaSeperateString(List<string> ids)
+        {
+            string idsCommaSeparatedList = "";
+            foreach (string id in ids)
+            {
+                idsCommaSeparatedList += id + ",";
+            }
+            return idsCommaSeparatedList.Substring(0, idsCommaSeparatedList.Length - 1);
+        }
+
+        #region constructors
+
+        public FollowApi(HttpClient httpClient, IAccessTokenProvider accessTokenProvider) : base(httpClient, accessTokenProvider)
+        {
+            SearchApi = new SearchApi(httpClient, accessTokenProvider);
+        }
+
+        public FollowApi(HttpClient httpClient, string accessToken) : base(httpClient, accessToken)
+        {
+            SearchApi = new SearchApi(httpClient, accessToken);
+        }
+
+        public FollowApi(HttpClient httpClient) : base(httpClient)
+        {
+            SearchApi = new SearchApi(httpClient);
+        }
+
+        #endregion
+        protected internal virtual ISearchApi SearchApi { get; set; }
+
+        #region GetFollowingContains
+
+        /// <summary>
+        /// Check if current user follows artists or users.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="type">The type, either artist or user, the current user follows.</param>
+        /// <param name="ids">A comma-separated list of the artist or the user Spotify IDs to check. A maximum of 50 IDs can be sent in one request.</param>
+        /// <returns>List<bool></returns>
+        /// <remarks>
+        /// https://developer.spotify.com/documentation/web-api/reference/follow/check-current-user-follows/
+        /// </remarks>
+        public async Task<List<bool>> GetFollowingContains(
+            string username,
+            FollowApi.ContainsTypes type,
+            List<string> ids,
+            string accessToken = null
+            )
+        {
+            if (string.IsNullOrEmpty(username)) throw new ArgumentNullException("username");
+            
+            if (ids?.Count < 1 || ids?.Count > 50) throw new ArgumentNullException("ids");
+
+            string url = $"{BaseUrl}/me/following/contains?" + 
+                $"type={(type == ContainsTypes.Artist ? "artist" : "user")}&" +
+                $"ids={GetCommaSeperateString(ids)}";
+
+            return await GetModel<List<bool>>(url, accessToken);
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/SpotifyApi.NetCore/IFollowApi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SpotifyApi.NetCore
+{
+    /// <summary>
+    /// Defines a wrapper for the Spotify Web Playlists API.
+    /// </summary>
+    public interface IFollowApi
+    {
+        #region GetFollowingContains
+
+        /// <summary>
+        /// Check if current user follows artists or users.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="type">The type, either artist or user, the current user follows.</param>
+        /// <param name="ids">A comma-separated list of the artist or the user Spotify IDs to check. A maximum of 50 IDs can be sent in one request.</param>
+        /// <returns>List<bool></returns>
+        /// <remarks>
+        /// https://developer.spotify.com/documentation/web-api/reference/follow/check-current-user-follows/
+        /// </remarks>
+        Task<List<bool>> GetFollowingContains(
+            string username,
+            FollowApi.ContainsTypes type,
+            List<string> ids,
+            string accessToken = null
+            );
+
+        #endregion
+    }
+}
