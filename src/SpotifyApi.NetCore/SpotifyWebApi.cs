3328eba972e6905efea8d60759892c6d18e993b2
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2020-06-27 00:26:25 +0530

    Fixed I think.

--- src/SpotifyApi.NetCore/SpotifyWebApi.cs
+++ src/SpotifyApi.NetCore/SpotifyWebApi.cs
@@ -173,10 +173,10 @@ namespace SpotifyApi.NetCore
             switch (verb)
             {
                 case "PUT":
-                    response = await _http.PutAsync(uri, content);
+                    response = await _http.PutAsync(uri, data == null ? null : content);
                     break;
                 case "POST":
-                    response = await _http.PostAsync(uri, content);
+                    response = await _http.PostAsync(uri, data == null ? null : content);
                     break;
                 default:
                     throw new NotSupportedException($"{verb} is not a supported verb");
