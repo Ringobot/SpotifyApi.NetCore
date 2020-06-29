1a94846d3f405b3a085d5a03d698618c9a84d83b
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2020-06-29 15:47:53 +0530

    Added some comments for my Helper Class code to assist in correctly working with existing PUT and POST requests without needing to change them.

--- src/SpotifyApi.NetCore/Helpers/SpotifyRequestDataWrappersHelper.cs
+++ src/SpotifyApi.NetCore/Helpers/SpotifyRequestDataWrappersHelper.cs
@@ -2,8 +2,14 @@
 
 namespace SpotifyApi.NetCore.Helpers
 {
+    /// <summary>
+    /// Helper for sending data via PUT and POST requests.
+    /// </summary>
     public class SpotifyRequestDataWrappersHelper
     {
+        /// <summary>
+        /// Helper struct for sending string[] which has to be in Json  called ids for PUT and POST requests.
+        /// </summary>
         public struct PassStringArrayIdsRequestDataJsonWrapperClass
         {
             [JsonProperty("ids")]
